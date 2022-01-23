903060e2c3ed1ea5c784cb53dc95077611593f80
Lim Geun <shing100@naver.com>
2022-01-23 21:41:12 +0900

    방 디테일 뷰 추가

--- README.md
+++ README.md
@@ -27,7 +27,8 @@ Cloning Airbnb with Python, Django, Tailwind and more...
 
 
 ## seed 
-- python manage.py seed_amenities
+- python manage.py seed_amenities 
+- python manage.py seed_facilities
 - python manage.py seed_users --number 50
 - python manage.py seed_rooms --number 100
 - python manage.py seed_reviews --number 200
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
-#from django.utils import timezonefrom django.shortcuts import render
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 class HomeView(ListView):
@@ -21,5 +21,8 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    print(pk)
-    return render(request, "rooms/detail.html")
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
