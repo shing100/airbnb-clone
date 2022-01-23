fe6b5177dd9ab49fe637993a1565639477734d0e
Lim Geun <shing100@naver.com>
2022-01-23 22:05:12 +0900

    룸 상세 뷰 추가

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,4 @@
-from django.views.generic import ListView
-from django.http import Http404
-from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from . import models
 
 class HomeView(ListView):
@@ -20,10 +18,8 @@ class HomeView(ListView):
     #     return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
-        # return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
