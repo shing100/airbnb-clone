2611070e0ee6e7484ee85c7840aa3d6042747735
Lim Geun <shing100@naver.com>
2022-01-23 21:56:12 +0900

    404 페이지 추가

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from django.urls import reverse
+from django.http import Http404
 from django.shortcuts import render, redirect
 from . import models
 
@@ -25,4 +25,5 @@ def room_detail(request, pk):
         room = models.Room.objects.get(pk=pk)
         return render(request, "rooms/detail.html", {"room": room})
     except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+        raise Http404()
+        # return redirect(reverse("core:home"))
