1989b692014989a97ab870bd0d473e2485a50abb
Lim Geun <shing100@naver.com>
2022-01-25 18:47:29 +0900

    User 로그인 로그아웃 추가

--- users/views.py
+++ users/views.py
@@ -1,9 +1,27 @@
-from django.views import View
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, reverse
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-class LoginView(View):
-    def get(self, request):
-        return render(request, "users/login.html")
+from . import forms
 
-    def post(self, request):
-        pass
+
+class LoginView(FormView):
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
