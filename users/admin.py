d63c2bfe8cdbcea1639dbc2c360382d00abd1d82
Lim Geun <shing100@naver.com>
2022-02-08 19:39:34 +0900

    깃헙 로그인 토큰 가져오기

--- users/admin.py
+++ users/admin.py
@@ -21,6 +21,7 @@ class CustomUserAdmin(UserAdmin):
                     "superhost",
                     "email_verified",
                     "email_secret",
+                    "login_method",
                 )
             },
         ),
@@ -39,4 +40,5 @@ class CustomUserAdmin(UserAdmin):
         "superhost",
         "is_staff",
         "is_superuser",
+        "login_method",
     )
