197722f9152aef25341a9d05946a9698fa5f67df
Lim Geun <shing100@naver.com>
2022-02-08 19:36:30 +0900

    깃험 로그인 1

--- users/views.py
+++ users/views.py
@@ -1,17 +1,40 @@
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import redirect, reverse
-from django.urls import reverse_lazy
+import os
 from django.views.generic import FormView
-
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms, models
 
 
 class LoginView(FormView):
+
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {"first_name": "Nicoas", "last_name": "Serr", "email": "itn@las.com"}
+
+    def form_valid(self, form):
+        form.save()
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
         user = authenticate(self.request, username=email, password=password)
@@ -34,18 +57,13 @@ def complete_verification(request, key):
     return redirect(reverse("core:home"))
 
 
-def log_out(request):
-    logout(request)
-    return redirect(reverse("core:home"))
+def github_login(request):
+    client_id = os.environ.get("GH_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/github/callback"
+    return redirect(
+        f"https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=read:user"
+    )
 
 
-class SignUpView(FormView):
-
-    template_name = "users/signup.html"
-    form_class = forms.SignUpForm
-    success_url = reverse_lazy("core:home")
-    initial = {"first_name": "geun", "last_name": "lim", "email": "shing100@naver.com"}
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+def github_callback(request):
+    pass
